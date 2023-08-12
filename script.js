85196e38172038c8980445c05a041c9d02bf5b26
anushaa15 <anushasaini2002@gmail.com>
2023-08-12 22:52:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,116 @@
+//main 1
+
+$(".header__card--1").click(function() {
+    $(".header__card").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cards").css("background-color", '#FBE7C6');
+    $(".cards__card").removeClass("card--active");
+    $(".cards__card--1").addClass("card--active");
+    $(".cards__card").css({
+      opacity: '0%'
+    });
+    $(".cards__card--1").css({
+      opacity: '100%'
+    });
+  });
+  
+  $(".header__card--2").click(function() {
+    $(".header__card").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cards").css("background-color", '#B4F8C8');
+    $(".cards__card").removeClass("card--active");
+    $(".cards__card--2").addClass("card--active");
+    $(".cards__card").css({
+      opacity: '0%'
+    });
+    $(".cards__card--2").css({
+      opacity: '100%'
+    });
+  });
+  
+  $(".header__card--3").click(function() {
+    $(".header__card").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cards").css("background-color", '#A0E7E5');
+    $(".cards__card").removeClass("card--active");
+    $(".cards__card--3").addClass("card--active");
+    $(".cards__card").css({
+      opacity: '0%'
+    });
+    $(".cards__card--3").css({
+      opacity: '100%'
+    });
+  });
+  
+  $(".header__card--4").click(function() {
+    $(".header__card").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cards").css("background-color", '#FFAEBC');
+    $(".cards__card").removeClass("card--active");
+    $(".cards__card--4").addClass("card--active");
+    $(".cards__card").css({
+      opacity: '0%'
+    });
+    $(".cards__card--4").css({
+      opacity: '100%'
+    });
+  });
+
+
+//main 2
+
+$(".header__card--1B").click(function() {
+    $(".header__cardB").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cardsB").css("background-color", '#FBE7C6');
+    $(".cards__cardB").removeClass("card--active");
+    $(".cards__card--1B").addClass("card--active");
+    $(".cards__cardB").css({
+      opacity: '0%'
+    });
+    $(".cards__card--1B").css({
+      opacity: '100%'
+    });
+  });
+  
+  $(".header__card--2B").click(function() {
+    $(".header__cardB").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cards").css("background-color", '#B4F8C8');
+    $(".cards__cardB").removeClass("card--active");
+    $(".cards__card--2B").addClass("card--active");
+    $(".cards__cardB").css({
+      opacity: '0%'
+    });
+    $(".cards__card--2B").css({
+      opacity: '100%'
+    });
+  });
+  
+  $(".header__card--3B").click(function() {
+    $(".header__cardB").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cards").css("background-color", '#A0E7E5');
+    $(".cards__cardB").removeClass("card--active");
+    $(".cards__card--3B").addClass("card--active");
+    $(".cards__cardB").css({
+      opacity: '0%'
+    });
+    $(".cards__card--3B").css({
+      opacity: '100%'
+    });
+  });
+  
+  $(".header__card--4B").click(function() {
+    $(".header__cardB").removeClass("header-active");
+    $(this).addClass("header-active");
+    $(".cards").css("background-color", '#FFAEBC');
+    $(".cards__cardB").removeClass("card--active");
+    $(".cards__card--4B").addClass("card--active");
+    $(".cards__cardB").css({
+      opacity: '0%'
+    });
+    $(".cards__card--4B").css({
+      opacity: '100%'
+    });
+  });
